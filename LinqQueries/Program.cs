e9fa469c625ab06f695a037cfb636460ab06f571
jako5457 <jako5457@gmail.com>
2022-09-15 08:37:40 +0200

    OK

--- LinqQueries/Program.cs
+++ LinqQueries/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Lav en LINQ query med Where og en tilhørende iteration
+// 2. Lav egen LINQ operator kaldet Filter i en ny klasse kaldet MyLinq
+
+
+using LinqQueries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "The Dark Knight", Rating=8.9f, Year = 2008 },
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 },
+                new Movie { Title = "Casablanca", Rating=8.5f, Year = 1942 },
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 }
+            };
+
+            //var query = movies.Where(m => m.Year > 2000);
+            var query = movies.Filter(m => m.Year > 2000);
+
+            foreach (Movie movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+    }
+}
+
